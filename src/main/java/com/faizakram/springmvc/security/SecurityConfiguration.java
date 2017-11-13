987f9a0a8472f5c9cd18cba1f65f54add235a6b8
faizakram <faiz.krm@gmail.com>
2017-11-13 12:21:24 +0530

    Commited

--- src/main/java/com/faizakram/springmvc/security/SecurityConfiguration.java
+++ src/main/java/com/faizakram/springmvc/security/SecurityConfiguration.java
@@ -36,6 +36,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
+		http.headers().defaultsDisabled().cacheControl();
 		http.authorizeRequests().antMatchers("/", "/list")
 				.access("hasRole('USER') or hasRole('ADMIN') or hasRole('DBA')")
 				.antMatchers("/delete-user-*").access("hasRole('ADMIN')").antMatchers("/edit-user-*")
